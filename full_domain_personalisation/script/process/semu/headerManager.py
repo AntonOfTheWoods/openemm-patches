6547520c230d28a072d20c6d7dcb1b217fb252fa
Anton Melser <melser.anton@gmail.com>
2013-08-23 07:59:19 +0200

    convert to using the message-id which is MUCH more useful

--- full_domain_personalisation/script/process/semu/headerManager.py
+++ full_domain_personalisation/script/process/semu/headerManager.py
@@ -4,9 +4,58 @@ __aps__ = {
         'uri':          None,
         'urimatrix':    None
 }
-import re
-#
+import re, sys
+
+try:
+        import agn
+except:
+        sys.path.append('/home/openemm/bin/scripts/')
+        import agn
+
 def handleOutgoingMail (ctx, mail):
+        precedence = __aps__['precedence']
+        if precedence:
+                found = None
+                for line in mail.head:
+                        if line.lower().startswith('precedence'):
+                                found = line
+                                break
+        if found is None:
+                mail.head.append('Precedence: %s' % precedence)
+
+        domain = mail.sender.rsplit('@', 1)[1]
+
+        re_str = r"Message-ID: <(?P<uidstr>.*)@.*>"
+        reg = re.compile(re_str)
+        uidstr = None
+        for header in mail.head:
+                if header.lower().startswith('message-id:'):
+                        #print header
+                        m = reg.search(header)
+                        uidstr = m.group("uidstr")
+                        break
+        if not uidstr is None:
+                uid = agn.UID ()
+                try:
+                        uid.parseUID (uidstr)
+                except Exception as ex:
+                        print ex
+
+
+        xjob = __aps__['x-job']
+        if xjob:
+                if not uid is None and uid.mailingID > 0:
+                        mail.head.append('X-job: ' + xjob % str(uid.mailingID))
+
+        listid = __aps__['list-id']
+        if listid:
+                if not uid is None and uid.mailingID > 0:
+                        mail.head.append('List-ID: ' + listid % (str(uid.companyID), domain))
+
+        abusereports = __aps__['abuse-reports-to']
+        if abusereports:
+                mail.head.append('X-Abuse-Reports-To: ' + abusereports % domain)
+
         urimatrix = __aps__['urimatrix']
         uri = __aps__['uri']
         if urimatrix or uri:
@@ -48,15 +97,19 @@ if __name__ == '__main__':
                         pass
                 mail = struct ()
                 mail.head = []
+                mail.head.append('Message-ID: <20130823020049-1.1.h.b.0.pp89lw4y80@news.gnlv.fr>')
                 mail.sender = 'news@toto.com'
                 mail.receiver = 'someone@somewhere.com'
-                __aps__['uri'] = 'http://localhost/unsubscribe?%(urlrecv)s'
+                __aps__['precedence'] = 'bulk'
+                __aps__['x-job'] = '%s'
+                __aps__['list-id'] = '<%s.%s>'
+                __aps__['abuse-reports-to'] = '<abuse@%s>'
                 handleOutgoingMail (None, mail)
                 print mail.head[0]
-                
-                mail.head = []
-                __aps__['urimatrix'] = 'news.example.com|mailto:DUN-%(urlrecv)s@lu.example.com|http://news.example.com?%(urlrecv)s\nletter.com|mailto:ext-%(urlrecv)s@localhost|http://localhost?%(urlrecv)s'
-                handleOutgoingMail (None, mail)
-                print mail.head[0]
+                print mail.head[1]
+                print mail.head[2]
+                print mail.head[3]
+                print mail.head[4]
+                                        
 
         _main ()
